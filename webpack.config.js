d423eacb8c65d95983bad98cb7d886aa9d51a87b
leonardo gutierrez <lgutierrez@flukyfactory.com>
2018-03-23 10:41:37 -0600

    -[Add] - Started webpack Setup
    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+//Load Dependencies ==========================
+const webpack = require('webpack');
+const glob = require('glob');
+const path = require('path');
+
+const views = glob.sync('./static/src/js/views/**/*.js');
+
+
+//Setting Internals ==========================
+const internals = {
+	static: `${ __dirname }/static`
+};
+internals.src = `${ internals.static }/src`;
+
+//Main Config ================================
+const config = {
+	entry: {
+		main : [`${ internals.src }/js/main.js`]
+	},
+	output: {
+		filename: '[name].js',
+	    path: path.resolve(`${__dirname}/static/`, 'js')
+	    //publicPath:  path.resolve(`${__dirname}/static/`, 'js')
+	}
+};
+
+if (views.length) {
+  for (var i = 0; i < views.length; i++) {
+    config.entry[views[i].replace('./static/src/js/','./').replace('.js','')] = views[i];
+  }
+};
+
+// Exposing `config` for webpack ================
+module.exports = config;
